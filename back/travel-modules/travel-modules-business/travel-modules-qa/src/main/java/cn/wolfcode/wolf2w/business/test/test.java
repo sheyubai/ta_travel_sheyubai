abb9437a894382ac6eb646178962266a3755946b
sheyubai <2357812232@qq.com>
2025-08-25 19:24:01 +0800

    测试推送远程

--- back/travel-modules/travel-modules-business/travel-modules-qa/src/main/java/cn/wolfcode/wolf2w/business/test/test.java
+++ back/travel-modules/travel-modules-business/travel-modules-qa/src/main/java/cn/wolfcode/wolf2w/business/test/test.java
@@ -2,7 +2,7 @@ package cn.wolfcode.wolf2w.business.test;
 
 public class test {
     public static void main(String[] args) {
-        //测试推送
+        //测试推送11
         System.out.println(1);
 
     }
